c676d2e4335626889281d089b72b314f58a73591
Sean Enck <sean@ttypty.com>
2024-09-29 10:42:50 -0400

    init go code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/seanenck/alpine-iso-builder
+
+go 1.23.1
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"iter"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type (
+	Config struct {
+		Tag        string `yaml:"tag"`
+		Repository struct {
+			URL          string   `yaml:"url"`
+			Repositories []string `yaml:"repositories"`
+		} `yaml:"repository"`
+		Architecture string `yaml:"architecture"`
+		Name         string `yaml:"name"`
+		Source       struct {
+			Remote    string   `yaml:"remote"`
+			Directory string   `yaml:"directory"`
+			Template  []string `yaml:"template"`
+		} `yaml:"source"`
+		Output    string `yaml:"output"`
+		World     string `yaml:"world"`
+		Timestamp string `yaml:"timestamp"`
+	}
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	inConfig := flag.String("config", "", "configuration file")
+	inTag := flag.String("tag", "", "alpine tag")
+	flag.Parse()
+	b, err := os.ReadFile(*inConfig)
+	if err != nil {
+		return err
+	}
+	cfg := Config{}
+	if err := yaml.Unmarshal(b, &cfg); err != nil {
+		return err
+	}
+
+	dir, err := os.MkdirTemp("", "alpine-iso.")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(dir)
+	var tag string
+	inputTag := *inTag
+	if inputTag == "" {
+		tag = cfg.Tag
+	}
+	rawTag := tag
+	if strings.Contains(rawTag, ".") {
+		parts := strings.Split(tag, ".")
+		if len(parts) != 3 {
+			return fmt.Errorf("version must be X.Y.Z (have: %s)", parts)
+		}
+		for _, p := range parts {
+			if _, err := strconv.Atoi(p); err != nil {
+				return fmt.Errorf("invalid version, non-numeric? %v (%w)", parts, err)
+			}
+		}
+		tag = fmt.Sprintf("v%s", strings.Join(parts[0:2], "."))
+	} else {
+		if rawTag != "edge" {
+			return fmt.Errorf("unknown version/not edge: %s", rawTag)
+		}
+	}
+	i, err := readFileToTrimmedLines(cfg.World)
+	if err != nil {
+		return err
+	}
+	var world []string
+	for line := range i {
+		if line != "" {
+			world = append(world, line)
+		}
+	}
+	obj := struct {
+		Name  string
+		Arch  string
+		World string
+		Tag   string
+	}{cfg.Name, cfg.Architecture, strings.Join(world, " "), tag}
+	t, err := template.New("t").Parse(strings.Join(cfg.Source.Template, "\n"))
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	if err := t.Execute(&buf, obj); err != nil {
+		return err
+	}
+	var repositories []string
+	url := cfg.Repository.URL
+	for _, repo := range cfg.Repository.Repositories {
+		r, err := template.New("t").Parse(fmt.Sprintf("%s/%s", url, repo))
+		if err != nil {
+			return err
+		}
+		var text bytes.Buffer
+		if err := r.Execute(&text, obj); err != nil {
+			return err
+		}
+
+		repositories = append(repositories, "--repository", text.String())
+	}
+
+	clone := filepath.Join(dir, "aports")
+	cmd := exec.Command("git", "clone", "--depth=1", cfg.Source.Remote, clone)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = dir
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	root := filepath.Join(clone, cfg.Source.Directory)
+	profile := filepath.Join(root, fmt.Sprintf("mkimg.%s.sh", cfg.Name))
+	if err := os.WriteFile(profile, buf.Bytes(), 0o755); err != nil {
+		return err
+	}
+
+	out := filepath.Join(dir, "iso")
+	if err := os.Mkdir(out, 0o755); err != nil {
+		return err
+	}
+	args := []string{
+		filepath.Join(root, "mkimage.sh"),
+		"--outdir", out,
+		"--arch", cfg.Architecture,
+		"--profile", cfg.Name,
+		"--tag", rawTag,
+	}
+	args = append(args, repositories...)
+	fmt.Println(args)
+	cmd = exec.Command("sh", args...)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	files, err := os.ReadDir(out)
+	if err != nil {
+		return nil
+	}
+	found := false
+	home := os.Getenv("HOME")
+	to := filepath.Join(home, cfg.Output)
+	now := time.Now().Format(cfg.Timestamp)
+	for _, f := range files {
+		name := f.Name()
+		if strings.HasSuffix(name, ".iso") {
+			fmt.Printf("artifact: %s\n", name)
+			found = true
+			dest := filepath.Join(to, fmt.Sprintf("%s.%s", now, name))
+			if err := exec.Command("mv", filepath.Join(out, name), dest).Run(); err != nil {
+				return err
+			}
+		}
+	}
+	if !found {
+		return errors.New("no built iso found?")
+	}
+	return nil
+}
+
+func readFileToTrimmedLines(file string) (iter.Seq[string], error) {
+	b, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	text := strings.Split(string(b), "\n")
+	return func(yield func(f string) bool) {
+		for _, line := range text {
+			t := strings.TrimSpace(line)
+			if !yield(t) {
+				return
+			}
+		}
+	}, nil
+}
